f2e20e8df9bcc04c55983e43cf044613176c02e5
tuannguyen <coixaygio107@gmail.com>
2024-12-03 19:41:13 +0700

    add filter search by genres

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const jsonServer = require("json-server");
-const cors = require('cors');
+const cors = require("cors");
 const path = require("path");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-
 const normalizeString = (str) =>
-  str?.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-
+  str
+    ?.normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
 
 server.use((req, res, next) => {
   if (req.method === "GET" && req.query.keyword) {
     const keyword = normalizeString(req.query.keyword);
     const data = router.db.get("results").value();
 
-   
     const filteredData = data.filter((item) => {
-      
       const isInTitle = normalizeString(item.title || "").includes(keyword);
 
-      
       const isInAuthors =
         Array.isArray(item.authors) &&
         item.authors.some((author) => {
-          return (
-            normalizeString(author.name || "").includes(keyword)
-          );
+          return normalizeString(author.name || "").includes(keyword);
         });
 
-      return isInTitle || isInAuthors;
+      const isInGenres =
+        Array.isArray(item.bookshelves) &&
+        item.bookshelves.some((genre) => {
+          return normalizeString(genre || "").includes(keyword);
+        });
+
+      return isInTitle || isInAuthors || isInGenres;
     });
 
     if (filteredData.length > 0) {
@@ -43,12 +45,13 @@ server.use((req, res, next) => {
   }
 });
 
-server.use(cors({
-  origin: '*'
-}));
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 server.use(middlewares);
 server.use(router);
-
 
 server.listen(8080, () => {
   console.log("JSON Server is running on http://localhost:8080");
